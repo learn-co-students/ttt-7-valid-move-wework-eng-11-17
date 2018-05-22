da515d1db3e13c7e4a7e26e92a13de7d74da1c19
Gonzalo Esperon <gonzalo.esperon@wework.com>
2018-05-22 19:52:31 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
-board = ["",""," "," ","X"," "," ","O",""]
-puts "Enter a position on the board 1-9:"
-user_input = gets.chomp
-move = user_input.to_i - 1
-
-def valid_move?(move, board)
-  move.between?(0.8) && !(position_taken?(board, move))
+board = ["","X"," "," "," "," "," "," "," "]
+# code your #valid_move? method here
+def valid_move?(board, position)
+  position = position.to_i-1
+  if position_taken?(board, position) == false && position < 9
+    true
+  end
 end
-def position_taken?(board, index)
-  ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, position)
+  # board[position] != " " && board[position] != "" && board[position] != "nil"  && board[position] == "X" || board[position] =="O"
+  ! (board[position] ==  " " || board[position] == "" || board[position] == nil) && board[position] == "X" || board[position] =="O"
 end
-
-valid_move?(move, board)
